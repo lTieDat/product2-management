75e5d6bf8050b38c4927e71350ef27b456672ce1
Lê Tiến Đạt <DatLT.B21VT013@stu.ptit.edu.vn>
2024-02-15 10:41:23 +0700

    logout

--- router/client/user.route.js
+++ router/client/user.route.js
@@ -9,5 +9,5 @@ router.get('/register', controller.register)
 router.post('/register', userValidate.registerPost, controller.registerPost)
 router.get('/login', controller.login)
 router.post('/login', userValidate.loginPost, controller.loginPost)
-
+router.get('/logout', controller.logout)
 module.exports = router;
