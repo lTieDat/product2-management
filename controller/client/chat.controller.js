a277b3279f73bd5fd93e6b4c7a6b4aec0eac6721
Lê Tiến Đạt <DatLT.B21VT013@stu.ptit.edu.vn>
2024-02-18 17:58:01 +0700

    optimize socket io in chat

--- controller/client/chat.controller.js
+++ controller/client/chat.controller.js
@@ -1,41 +1,15 @@
 const Chat = require('../../models/chat.model');
 const User = require('../../models/user.model');
 const uploadToCloudinary = require('../../helpers/uploadToCloudinary');
+const socket = require('../../sockets/client/chat.socket');
+
+
 // [GET] /chat
 module.exports.index = async(req, res) => {
-    const userId = res.locals.user.id;
-    const fullName = res.locals.user.fullName;
-    _io.once('connection', (socket) => {
-        socket.on("CLIENT_SEND_MESSAGE", async (data)=>{
-            let images = [];
-            for (const item of data.images) {
-                const link = await uploadToCloudinary(item);
-                images.push(link);
-            }
-            const chat = new Chat({
-                user_id: userId,
-                content: data.content,
-                images: images
-            });
-            await chat.save();
-            // show data in client
-            _io.emit("SERVER_RETURN_MESSAGE", {
-                userId: userId,
-                fullName: fullName,
-                content: data.content,
-                images: images 
-            });
-            //end show data in client
-        });
-        // typing
-        socket.on("CLIENT_TYPING", async (data)=>{
-            socket.broadcast.emit("SERVER_TYPING", {
-                userId: userId,
-                fullName: fullName,
-                type: data
-            });
-        });
-    });
+    // socket IO
+    socket(res);
+    // end socket IO
+    
     // get all chat
     const chat = await Chat.find({
         deleted: false
